f1a737fa673e6c37d1f3b3b145d9e292e4cd3149
SahanaUdaya <146607838+SahanaUdaya@users.noreply.github.com>
2023-10-16 20:55:36 +0530

    update

--- assignment4/script.js
+++ assignment4/script.js
@@ -1,20 +1,20 @@
 var names=new Array();
-names[0]="Sana";
-names[1]="Adhvi";
-names[2]="Jesh";
+names[0]="Yaakov";
+names[1]="John";
+names[2]="Jen";
 names[3]="jason";
-names[4]="riya";
-names[5]="reena";
-names[6]="Reeta";
-names[7]="Samya";
-names[8]="jeera";
-names[9]="jam";
+names[4]="paul";
+names[5]="frank";
+names[6]="larry";
+names[7]="paula";
+names[8]="laura";
+names[9]="jim";
 
 
 for (var i = 0; i < names.length; i++) {
 
 	
-        if(names[i].charAt(0)==='S'|| names[i].charAt(0)==='s'){
+        if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
         console.log("Goodbye "+ names[i])
 	}
 	else{
