7f0006aa43fb33c2256ed6eb4e357b8a1c1e339f
SahanaUdaya <146607838+SahanaUdaya@users.noreply.github.com>
2023-10-16 19:52:28 +0530

    submit

---
+++ assignment4/script.js
@@ -0,0 +1,24 @@
+var names=new Array();
+names[0]="Sana";
+names[1]="Adhvi";
+names[2]="Jesh";
+names[3]="jason";
+names[4]="riya";
+names[5]="reena";
+names[6]="Reeta";
+names[7]="Samya";
+names[8]="jeera";
+names[9]="jam";
+
+
+for (var i = 0; i < names.length; i++) {
+
+	
+        if(names[i].charAt(0)==='S'|| names[i].charAt(0)==='s'){
+        console.log("Goodbye "+ names[i])
+	}
+	else{
+		console.log("Hello "+ names[i])
+	}
+	
+}
